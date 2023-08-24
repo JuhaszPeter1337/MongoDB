e7ba54ca5fba97fa1e3e9be076dbd7c65c5f33e0
Peter Juhasz <juhasz.peter1337@gmail.com>
2023-08-24 16:28:54 +0200

    Changed person structure

--- person.py
+++ person.py
@@ -1,7 +1,10 @@
 class Person:
-    def __init__(self, name, age, school, grade, number):
+    def __init__(self, _id, name, age, school, _class, number, grades):
+        self._id = _id
         self.name = name
         self.age = age
         self.school = school
-        self.grade = grade
+        self._class = _class
         self.number = number
+        self.grades = grades
+
